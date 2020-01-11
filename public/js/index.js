3af30c2bccc9cd76dd3dc07c4cf0108e2f23ee0d
Esa <diapaasi@gmail.com>
2020-01-12 01:26:07 +0200

    Added loading file contents into gps.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,25 @@
-function sendGPSData() {
-  console.log("Loading gps file");
+var gps = new GPS;
 
-  document.getElementById("gps-file").disabled = true;
-  document.getElementById("open-button").disabled = true;
-
-  document.getElementById("spinner").style.display = "block";
-  let gpsData = document.getElementById("gps-file").files[0];
+async function sendGPSData() {
+  if(document.getElementById("gps-file").files.length > 0){
+    console.log("Loading gps file");
+  
+    document.getElementById("gps-file").disabled = true;
+    document.getElementById("open-button").disabled = true;
+  
+    document.getElementById("spinner").style.display = "block";
+    const gpsFile = document.getElementById("gps-file").files[0];
+    const gpsData = await gpsFile.text();
+    gpsData.split('\n').map((value)=>{
+      gps.update(value);
+    });
+    document.getElementById("spinner").style.display = "none";
+    console.log("Loading complete");
+  }
 }
+
+gps.on('data', function(parsed) {
+  console.log(parsed);
+});
+
 document.getElementById("open-button").addEventListener("click", sendGPSData);
