7cbd0399e2c1dffceaceb8117ef53fb11438e71f
Esa <diapaasi@gmail.com>
2020-01-12 00:54:08 +0200

    Updated express port to work with Heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(express.static('public'));
 
-app.listen(port, () => console.log(`GPS viewer is running`));
+app.listen(PORT, () => console.log(`GPS viewer listening on ${PORT}`));
