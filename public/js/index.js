df5d081f7a378d9e0f4331982cb81db89931638b
Esa <diapaasi@gmail.com>
2020-02-02 22:06:35 +0200

    Refactored code Added leaflet.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,16 @@
-var gps = new GPS;
+var gps = new GPS();
 
 async function sendGPSData() {
-  if(document.getElementById("gps-file").files.length > 0){
+  if (document.getElementById("gps-file").files.length > 0) {
     console.log("Loading gps file");
-  
+
     document.getElementById("gps-file").disabled = true;
     document.getElementById("open-button").disabled = true;
-  
+
     document.getElementById("spinner").style.display = "block";
     const gpsFile = document.getElementById("gps-file").files[0];
     const gpsData = await gpsFile.text();
-    gpsData.split('\n').map((value)=>{
+    gpsData.split("\n").map(value => {
       gps.update(value);
     });
     document.getElementById("spinner").style.display = "none";
@@ -18,8 +18,13 @@ async function sendGPSData() {
   }
 }
 
-gps.on('data', function(parsed) {
+gps.on("data", function(parsed) {
   console.log(parsed);
 });
 
 document.getElementById("open-button").addEventListener("click", sendGPSData);
+
+var map = L.map("map", {
+  center: [51.505, -0.09],
+  zoom: 13
+});
