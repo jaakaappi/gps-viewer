78aae01c5fc859192d92a6de9ddb521c1ddd35f1
Esa <diapaasi@gmail.com>
2020-03-21 22:58:55 +0200

    Added path visualization

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ require("./mmlLayers");
 const GPS = require("gps");
 
 var gps = new GPS();
+var path = [];
 
 async function sendGPSData() {
   if (document.getElementById("gps-file").files.length > 0) {
@@ -16,22 +17,34 @@ async function sendGPSData() {
     document.getElementById("spinner").style.display = "block";
     const gpsFile = document.getElementById("gps-file").files[0];
     const gpsData = await gpsFile.text();
+
     gpsData.split("\n").map(value => {
       gps.update(value);
     });
+    var polyline = L.polyline(path, {
+      color: "#083600",
+      weight: 4,
+      dashArray: "10,10"
+    }).addTo(map);
+
+    map.fitBounds(polyline.getBounds());
+
     document.getElementById("spinner").style.display = "none";
     console.log("Loading complete");
   }
 }
 
-gps.on("data", function(parsed) {
-  console.log(parsed);
-});
-
 document.getElementById("open-button").addEventListener("click", sendGPSData);
 
 var map = new L.map("map", {
   crs: L.TileLayer.MML.get3067Proj()
-}).setView([61, 25], 6);
+}).setView([61, 25], 4);
 
 L.tileLayer.mml_wmts({ layer: "maastokartta" }).addTo(map);
+
+gps.on("data", function(parsed) {
+  if (["GGA", "RMC", "GLL"].includes(parsed.type)) {
+    console.log(parsed.lat, parsed.lon);
+    path.push([parsed.lat, parsed.lon]);
+  }
+});
